576ebc197248d83df8d4b77e030f06d85ae0aff9
mxpichardo <mayobanexpichardo@gmail.com>
2021-02-13 19:57:35 -0400

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,89 @@
+/*!
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+    */
+    (function ($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+                this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length
+                ? target
+                : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate(
+                    {
+                        scrollTop: target.offset().top - 72,
+                    },
+                    1000,
+                    "easeInOutExpo"
+                );
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").click(function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#mainNav",
+        offset: 74,
+    });
+
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+})(jQuery); // End of use strict
+
+
+
+//Elemento whatsapp
+popupWhatsApp = () => {
+  
+    let btnClosePopup = document.querySelector('.closePopup');
+    let btnOpenPopup = document.querySelector('.whatsapp-button');
+    let popup = document.querySelector('.popup-whatsapp');
+    let sendBtn = document.getElementById('send-btn');
+  
+    btnClosePopup.addEventListener("click",  () => {
+      popup.classList.toggle('is-active-whatsapp-popup')
+    })
+    
+    btnOpenPopup.addEventListener("click",  () => {
+      popup.classList.toggle('is-active-whatsapp-popup')
+       popup.style.animation = "fadeIn .6s 0.0s both";
+    })
+    
+    sendBtn.addEventListener("click", () => {
+    let msg = document.getElementById('whats-in').value;
+    let relmsg = msg.replace(/ /g,"%20");
+       
+     window.open('https://wa.me/5215571533815?text='+relmsg, '_blank'); 
+    
+    });
+  
+    setTimeout(() => {
+      popup.classList.toggle('is-active-whatsapp-popup');
+    }, 3000);
+  }
+  
+  popupWhatsApp();
